2515279786141d1364d764c4af952fd65b61e76d
Salzaufsauge <147203308+Salzaufsauge@users.noreply.github.com>
2025-05-12 21:00:10 +0200

    added basic tsp data import

---
+++ src/import_data/__init__.py
@@ -0,0 +1,22 @@
+import numpy as np
+
+# reads tsp files for now only supports 2D tsps
+def read_tsp(file):
+    with open(file, 'r') as f:
+        lines = f.readlines()
+        weight_type = None
+        found_node_coord_section = False
+        coords = []
+        for line in lines:
+            if line.startswith('EDGE_WEIGHT_TYPE'):
+                weight_type = line.split(':')[1].strip()
+                continue
+            if line.startswith('NODE_COORD_SECTION'):
+                found_node_coord_section = True
+                continue
+            if found_node_coord_section:
+                if line.startswith('EOF'):
+                    break
+                node_id, x, y = line.split()
+                coords.append((int(x), int(y)))
+        return weight_type, np.array(coords)
--- src/main.py
+++ src/main.py
@@ -1,2 +1,14 @@
+import numpy as np
+import pathlib as path
+import import_data
+
+def get_project_root() -> path.Path:
+    return path.Path(__file__).parent.parent
+
+def main():
+    tsp_files = get_project_root().glob('data/*.tsp')
+    for tsp_file in tsp_files:
+        print(import_data.read_tsp(tsp_file))
+
 if __name__ == '__main__':
-    print("Hello World!")
+    main()
